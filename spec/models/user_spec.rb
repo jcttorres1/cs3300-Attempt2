c8a0223d194e430412e331b604ef50b531cab010
Jake Torres <jtorres3@uccs.edu>
2022-04-13 01:42:22 +0000

    Setting up Procfile and Devise gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "validation tests" do
-    it "checks if user is present" do
-      user = User.new(description: "email of user")
-      expect(user.valid?).to eq(false)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
